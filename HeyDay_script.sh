bc769b88c93d5380b44cf36858fe267873b372ab
Yuki Yanagita <113555702+yyanagita66@users.noreply.github.com>
2024-10-09 01:51:58 -0400

    Add files via upload

---
+++ HeyDay_script.sh
@@ -0,0 +1,16 @@
+: << COMMENT
+This is a code to display hey day "imput name"
+Aauther: Yuki Yanagita
+Date of latest update: 10/09/2024
+COMMENT
+
+#!/bin/bash
+
+# take a input
+echo "What is your name?"
+read -t 10 name
+
+echo "Hey $name"
+
+
+
